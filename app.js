3f3b1ebaa761c6f927f0cfb2d03f4fdb18a9ae8f
HenryEdmodo <henry@edmodo.com>
2015-03-15 13:16:58 -0700

    all da stuff
    add it

---
+++ app.js
@@ -0,0 +1,14 @@
+// server.js
+var http = require("http");
+
+function greet(req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello World");
+    res.end();
+}
+
+var server = http.createServer(greet);
+
+server.listen(3000);
+
+gasrregr
