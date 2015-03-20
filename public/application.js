1520029c966b923f45f7af35c577e80d78a97c2f
HenryEdmodo <henry@edmodo.com>
2015-03-20 09:39:02 -0700

    Great stuff
    - Parse API response to JSON
    - style the page better
    - more events

--- public/application.js
+++ public/application.js
@@ -25,110 +25,27 @@ app.controller("wineController", function($scope, $http) {
       alert("broken");
      });
 */
-var hitAPI = function(word){
-    console.log('get here?');
-    $.ajax({
-        type: 'GET',
-        url: 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + word + '?key=99bb6d8c-33b2-42be-845c-5a08ff2d8e33',
-        success: function(word) {
-            doTheStuff(word);   
-            console.log(word);                 
-        },
-        error: function() {
-            alert("Something went wrong")
-        }
-    });
-}    
 
-var doTheStuff = function(word) {
-    console.log(word);
-          var xml = $(word);
-          var firstNode = xml.find('entry_list');
-          console.log(firstNode+ 'thjis');
-          var entry = firstNode.firstChild;  
-          console.log(entry);
-          //var content = entry.text();
-          //console.log(content);
-}
+
 
 $(document).ready(function() {
 
     // $('body').on('click', 'submit-button', hitAPI);
-
+/*
     $('#submit-button').click(function() {        
         var word = $('#the-word').val();
         console.log(word);      
         hitAPI(word);                       
-    });
-    $('.login-edmodo-button').click(loginEdmodo);   
-    $('#submit-button-two').click(showGroups);
+    });    
+*/
+
+    $('#submit-button-two').click(function() {        
+        var word = $('#the-word').val();
+        console.log(word);      
+        hitAPI(word);                       
+    });   
     
 });
-
-
-var loginEdmodo = function() {
-    window.open("https://api.edmodo.com/oauth/authorize?client_id=8d06878fb27b4913af74a7a12936bcebeacbf4b7aeef6fcc1aeb0c63a04996f6&redirect_uri=http://localhost:3000&scope=basic%20read_groups&response_type=token", "_blank");       
-};
-
-var getUrlParameter = function(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-    }
-}     
-var codeValue = getUrlParameter('code');
-console.log(codeValue);
-
-var getToken = function(codeValue) {
-    $.ajax({
-        type: "POST",
-        url: "https://api.edmodo.com/oauth/token",
-        data: {
-            "client_id": "8d06878fb27b4913af74a7a12936bcebeacbf4b7aeef6fcc1aeb0c63a04996f6",
-            "client_secret": "e88ef75723d56b78b4da600ff5d1e0c7212567c6471239fa92c09a663c676f65",
-            "redirect_uri": "http://localhost:3000",
-            "code": codeValue,
-            "grant_type": "authorization_code"
-        },      
-        success: function(response) {
-            console.log(response.access_token);             
-        },
-        dataType: "json"
-    });  
-}
-
-var getHashValue = function(key) {
-  return location.hash.match(new RegExp(key+'=([^&]*)'))[1];
-}
-
-var accessToken = getHashValue('access_token');
-
-var showGroups = function(access_token) {
-    console.log('got here');
-    
-    $.ajax({
-        type: "GET",
-        url: "https://api.edmodo.com/users/me?access_token="+accessToken,        
-        success: function(response) {
-            console.log(response.username);             
-        },
-        error: function() {
-            console.log('done messed up');
-        }        
-    });  
-
-}
-
-
-
-// getToken();
 
 var hitAPI = function(word){
     console.log('get here?');
@@ -136,8 +53,7 @@ var hitAPI = function(word){
         type: 'GET',
         url: 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + word + '?key=99bb6d8c-33b2-42be-845c-5a08ff2d8e33',
         success: function(word) {
-            doTheStuff(word);   
-            console.log(word);                 
+            doTheStuff(word);               
         },
         error: function() {
             alert("Something went wrong")
@@ -145,14 +61,94 @@ var hitAPI = function(word){
     });
 }    
 
-var doTheStuff = function(word) {
-    console.log(word);
-          var xml = $(word);
+var doTheStuff = function(word) {    
+    var xml2json = function(xml) {
+        try {
+            var obj = {};
+            if (xml.children.length > 0) {
+              for (var i = 0; i < xml.children.length; i++) {
+                var item = xml.children.item(i);
+                var nodeName = item.nodeName;
+
+                if (typeof (obj[nodeName]) == "undefined") {
+                  obj[nodeName] = xml2json(item);
+                } else {
+                  if (typeof (obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                  }
+                  obj[nodeName].push(xml2json(item));
+                }
+              }
+            } else {
+              obj = xml.textContent;
+            }
+            return obj;
+          } catch (e) {
+              // console.log(e.message);
+          }
+        }
+    var bringItOut = xml2json(word);
+    console.log(bringItOut);
+
+    var checkIfArray = function(obj, itemArr, itemObj) {        
+        if ($.isArray(obj)) {
+            variable = itemArr;   
+            console.log(itemArr);         
+        } else {
+            variable = itemObj;
+        }
+        return variable;
+    }  
+
+    var theEntry = checkIfArray(bringItOut.entry_list.entry, bringItOut.entry_list.entry[0].ew, bringItOut.entry_list.entry.ew); 
+    var partOfSpeech = checkIfArray(bringItOut.entry_list.entry, bringItOut.entry_list.entry[0].fl, bringItOut.entry_list.entry.fl); 
+    
+    if ($.isArray(bringItOut.entry_list.entry)) {
+        var definition = bringItOut.entry_list.entry[0].def.dt[0];   
+    } else {
+        var defintion = bringItOut.entry_list.entry.def.dt[0];
+    }
+
+
+    console.log("the word is: " + theEntry);
+
+    // var partOfSpeech = (bringItOut.entry_list.entry[0].fl);
+    console.log('part of speech is: ' + partOfSpeech);
+
+   // var definition =(bringItOut.entry_list.entry[0].def.dt[0]);
+    console.log("the definition is :" + definition);
+
+    $("#entry-one").text(theEntry);
+    $("#POS-one").text(partOfSpeech);
+    $("#def-one").text(definition);
+
+    $("#response").show();
+
+
+
+
+
+
+
+
+
+
+/*          var xml = $(word);
           var firstNode = xml.find('entry_list');
           console.log(firstNode);
-          var entry = firstNode.firstChild;  
-          console.log(entry);
+          var entry = firstNode[0];  
+          console.log("test is: " + entry);
           //var content = entry.text();
           //console.log(content);
+*/
+    var firstGuy = word.firstChild;
+    // console.log(firstGuy);
+    // var selected = firstGuy.getElementById(word);
+    // console.log(selected);
+    // firstGuy.SelectSingleNode()
+    var secondGuy = firstGuy.childNodes[0];
+    // console.log(secondGuy);
 }
-
